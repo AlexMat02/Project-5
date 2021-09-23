36769ce8dcbdb2bcbe9240248cca39c15bca148d
AlexMat02 <ourougank@gmail.com>
2021-09-23 09:31:51 +0200

    Made orderconfirmation page and finished the project

--- javascript/orderconfirmation.js
+++ javascript/orderconfirmation.js
@@ -1,6 +1,14 @@
 let listOfCartItems = JSON.parse(localStorage.getItem("listOfCartItems"));
 console.log(listOfCartItems);
 
+let teddybearData_deserialized = JSON.parse(localStorage.getItem("backendData"));
+console.log(teddybearData_deserialized);
+
+let products = [];
+for (let q = 0; q < listOfCartItems.length; q++){
+    products.push(listOfCartItems[q]._id)
+}
+
 let totalPrice = JSON.parse(localStorage.getItem("totalPrice"));
 
 let contact = JSON.parse(localStorage.getItem("formData"));
@@ -12,17 +20,24 @@ currentPrice.textContent = totalPrice + ".00 $";
 
 const htmlUserId = document.getElementById("orderId");
 
-const urlPOSTRequest = "http://127.0.0.1:3000/api/teddies/order";
-const urlBackendId = "http://127.0.0.1:3000/api/teddies/:_id";
+let body = {};
+body.contact = contact;
+body.products = products;
+console.log(body);
 
-fetch(urlPOSTRequest, {method : 'POST', body: contact, listOfCartItems})
+const urlPOSTRequest = "http://127.0.0.1:3000/api/teddies/order";
+
+fetch(urlPOSTRequest, {method : 'POST',headers: {
+    'Content-Type': 'application/json;charset=utf-8'
+  }, body: JSON.stringify(body)})
     .then(response => response.json()).then(data => {
     console.log(data);
-    console.log("c");
+    htmlUserId.textContent = data.orderId;
 });
 
-fetch(urlBackendId).then(response => response.json()).then(data => {
-    console.log(data);
-    console.log("w");
-    htmlUserId.textContent = data;
-});
+for (let i = 0; i < products.length; i++){
+    let urlBackendId = `http://127.0.0.1:3000/api/teddies/${products[i]}`;
+    fetch(urlBackendId).then(response => response.json()).then(data => {
+        console.log(data);
+    });
+};
