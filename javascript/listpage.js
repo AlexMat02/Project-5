644e282c5ede63b16e097a593864692968c2cbf7
AlexMat02 <ourougank@gmail.com>
2021-09-27 14:38:41 +0200

    Made listpage 100% dynamic

--- javascript/listpage.js
+++ javascript/listpage.js
@@ -18,8 +18,45 @@ fetch(urlBackend).then(response => response.json()).then(data => {
 let teddybearData_deserialized = JSON.parse(localStorage.getItem("backendData"))
 let productDisplay = 0;
 
+const mainContainerDiv = document.getElementsByClassName("main_container");
+// Function to create cards
+function listCreator(currentIndex) {
+    // Creates the main div
+    let mainDiv = document.createElement("div");
+    mainDiv.setAttribute("class", "container_teddybear");
+    // Creates the header
+    let header3 = document.createElement("h3");
+    header3.setAttribute("class", "container_teddybear--text")
+    header3.textContent = teddybearData_deserialized[currentIndex].name;
+    // Creates the second div
+    let imageDiv = document.createElement("div");
+    imageDiv.setAttribute("class", "container_teddybear_content")
+    imageDiv.style.display = "flex";
+    imageDiv.style.justifyContent = "end";
+    // Creates the img
+    let imageCreator = document.createElement("img");
+    imageCreator.setAttribute("class", "container_teddybear--img");
+    imageCreator.src = teddybearData_deserialized[currentIndex].imageUrl;
+    // Creates the link
+    let linkCreator = document.createElement("a");
+    linkCreator.href = "/html/singleproduct1.html";
+    linkCreator.setAttribute("class", "container_teddybear--buycue");
+    linkCreator.setAttribute("id", "product" + (currentIndex + 1));
+    // Creates the text
+    let textCreator = document.createElement("p");
+    textCreator.textContent = "Buy Here!";
+    // Creates the card
+    mainContainerDiv[0].appendChild(mainDiv);
+    mainDiv.appendChild(header3);
+    mainDiv.appendChild(imageDiv);
+    imageDiv.appendChild(imageCreator);
+    imageDiv.appendChild(linkCreator);
+    linkCreator.appendChild(textCreator);
+};
+
 // This is to create the listpage dynamically
 for (i = 1; i <= teddybearData_deserialized.length; i++) {
+    listCreator(i - 1);
     let currentTeddybear = document.getElementById('product' + i);
     {
         // J is for the index in the array, which is why it's i - 1 since i starts at 0
